004d668f60c8cc644eb46d725d9d90d77a482201
xulixin <henryviolet@sina.com>
2021-12-16 14:15:55 +0000

    [Added] 使用Java8以上版本的JDK中的BASE64加密字符串工具类

---
+++ webfast-common/src/main/java/com/jetwinner/util/StringEncoderUtil.java
@@ -0,0 +1,26 @@
+package com.jetwinner.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * @author xulixin
+ */
+public class StringEncoderUtil {
+
+    private StringEncoderUtil() {
+        // reserved.
+    }
+
+    public static String encode(String str) {
+        return Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String decode(Object obj) {
+        return obj == null ? null : decode(obj.toString());
+    }
+
+    public static String decode(String encodeStr) {
+        return new String(Base64.getDecoder().decode(encodeStr), StandardCharsets.UTF_8);
+    }
+}
