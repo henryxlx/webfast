78f21be2c5e97f9e3e33815fb1dbde0e09e3384a
xulixin <henryviolet@sina.com>
2021-12-17 06:58:50 +0000

    [Added] 后台管理小组设置页面原型、小组话题页面原型与控制器调用

--- webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/GroupController.java
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/GroupController.java
@@ -13,4 +13,14 @@ public class GroupController {
     public String indexPage() {
         return "/admin/group/index";
     }
+
+    @RequestMapping("/admin/group/set")
+    public String groupSetPage() {
+        return "/admin/group/set";
+    }
+
+    @RequestMapping("/admin/group/thread")
+    public String threadPage() {
+        return "/admin/group/thread";
+    }
 }
