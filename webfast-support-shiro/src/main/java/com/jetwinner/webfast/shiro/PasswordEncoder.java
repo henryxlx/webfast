0b08627441de3779d6a103d04931f76ee54165d1
xulixin <henryviolet@sina.com>
2021-12-20 11:42:37 +0000

    [Added] 使用Shiro内部机制完成字符串加密的应用工具类

---
+++ webfast-support-shiro/src/main/java/com/jetwinner/webfast/shiro/PasswordEncoder.java
@@ -0,0 +1,26 @@
+package com.jetwinner.webfast.shiro;
+
+import org.apache.shiro.crypto.hash.SimpleHash;
+
+/**
+ * @author xulixin
+ */
+public class PasswordEncoder {
+
+    private PasswordEncoder() {
+        // reserved.
+    }
+
+    static String encodePassword(String password, String salt) {
+        return encodePassword(password, salt, false);
+    }
+
+    static String encodePassword(String password, String salt, boolean isHexEncoded) {
+        // 算法名称
+        String algorithmName = ShiroConfig.DEFAULT_HASH_ALGORITHM_NAME;
+        // 加密次数
+        int times = ShiroConfig.DEFAULT_HASH_ITERATIONS;
+        SimpleHash simpleHash = new SimpleHash(algorithmName, password, salt, times);
+        return isHexEncoded ? simpleHash.toString() : simpleHash.toBase64();
+    }
+}
