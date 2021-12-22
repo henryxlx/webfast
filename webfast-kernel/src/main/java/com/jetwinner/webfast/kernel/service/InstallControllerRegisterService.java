3649584fe015ae1f5a6f37975deef93f8243890a
xulixin <henryviolet@sina.com>
2021-12-22 14:21:32 +0000

    [Added] 动态注册安装程序控制器，在没有正确设置数据源文件情况下注册安装控制器，成功安装后删除安装控制器防止误操作

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/kernel/service/InstallControllerRegisterService.java
@@ -0,0 +1,12 @@
+package com.jetwinner.webfast.kernel.service;
+
+/**
+ * @author xulixin
+ */
+public interface InstallControllerRegisterService {
+
+    public void addInstallControllerMapping() throws Exception;
+
+    public void removeInstallControllerMapping();
+
+}
---
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/install/InstallControllerRegisterServiceImpl.java
@@ -0,0 +1,68 @@
+package com.jetwinner.webfast.mvc.controller.install;
+
+import com.jetwinner.webfast.kernel.service.InstallControllerRegisterService;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+@Component
+public class InstallControllerRegisterServiceImpl implements InstallControllerRegisterService, ApplicationContextAware {
+
+    static final String INSTALL_CONTROLLER_BEAN_NAME = "webfastInstallController";
+
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addInstallControllerMapping() throws Exception {
+        requestMappingHandlerMapping = (RequestMappingHandlerMapping) applicationContext.getBean("requestMappingHandlerMapping");
+        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
+
+        Class<?> myController = ClassLoader.getSystemClassLoader().loadClass("com.jetwinner.webfast.mvc.controller.install.InstallController");
+        // 这里通过builder直接生成了installController的definition，然后注册进去
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(myController);
+        defaultListableBeanFactory.registerBeanDefinition(INSTALL_CONTROLLER_BEAN_NAME, beanDefinitionBuilder.getBeanDefinition());
+        Method method = requestMappingHandlerMapping.getClass().getSuperclass().getSuperclass().getDeclaredMethod("detectHandlerMethods", Object.class);
+        method.setAccessible(true);
+        method.invoke(requestMappingHandlerMapping, INSTALL_CONTROLLER_BEAN_NAME);
+    }
+
+    @Override
+    public void removeInstallControllerMapping() {
+        requestMappingHandlerMapping = (RequestMappingHandlerMapping) applicationContext.getBean("requestMappingHandlerMapping");
+        Object controller = applicationContext.getBean(INSTALL_CONTROLLER_BEAN_NAME);
+        if (controller == null) {
+            return;
+        }
+        Class<?> targetClass = controller.getClass();
+        ReflectionUtils.doWithMethods(targetClass, method -> {
+            Method specificMethod = ClassUtils.getMostSpecificMethod(method, targetClass);
+            try {
+                Method createMappingMethod = RequestMappingHandlerMapping.class.
+                        getDeclaredMethod("getMappingForMethod", Method.class, Class.class);
+                createMappingMethod.setAccessible(true);
+                RequestMappingInfo requestMappingInfo = (RequestMappingInfo)
+                        createMappingMethod.invoke(requestMappingHandlerMapping, specificMethod, targetClass);
+                if (requestMappingInfo != null) {
+                    requestMappingHandlerMapping.unregisterMapping(requestMappingInfo);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, ReflectionUtils.USER_DECLARED_METHODS);
+    }
+}
