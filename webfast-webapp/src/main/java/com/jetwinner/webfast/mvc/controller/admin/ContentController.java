d7cfb723e8399d487f414b9bdd8354f1292c57a5
xulixin <henryviolet@sina.com>
2021-12-27 14:53:30 +0000

    [Changed] 网站内容后台管理控制器添加内容服务访问功能

--- webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/ContentController.java
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/ContentController.java
@@ -1,6 +1,8 @@
 package com.jetwinner.webfast.mvc.controller.admin;
 
+import com.jetwinner.webfast.kernel.service.AppContentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller("webfastAdminContentController")
 public class ContentController {
 
+    private final AppContentService contentService;
+
+    public ContentController(AppContentService contentService) {
+        this.contentService = contentService;
+    }
+
     @RequestMapping("/admin/content")
-    public String indexPage() {
+    public String indexPage(Model model) {
+        model.addAttribute("contents", contentService.findAllContent());
         return "/admin/content/index";
     }
 }
