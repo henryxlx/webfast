6733c9cc63a0da24103c16a96c8ee3ac15c96a50
xulixin <henryviolet@sina.com>
2021-12-31 00:34:06 +0000

    [Fixed] 完成后台内容管理查询控制器默认查询显示方法的处理，正确处理各类数据查询注入到模型中

--- webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/ContentController.java
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/admin/ContentController.java
@@ -1,9 +1,19 @@
 package com.jetwinner.webfast.mvc.controller.admin;
 
+import com.jetwinner.webfast.kernel.Paginator;
+import com.jetwinner.webfast.kernel.service.AppCategoryService;
 import com.jetwinner.webfast.kernel.service.AppContentService;
+import com.jetwinner.webfast.kernel.service.AppUserService;
+import com.jetwinner.webfast.kernel.typedef.ParamMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author xulixin
@@ -12,14 +22,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContentController {
 
     private final AppContentService contentService;
+    private final AppUserService userService;
+    private final AppCategoryService categoryService;
 
-    public ContentController(AppContentService contentService) {
+    public ContentController(AppContentService contentService,
+                             AppUserService userService,
+                             AppCategoryService categoryService) {
+
         this.contentService = contentService;
+        this.userService = userService;
+        this.categoryService = categoryService;
     }
 
     @RequestMapping("/admin/content")
-    public String indexPage(Model model) {
-        model.addAttribute("contents", contentService.findAllContent());
+    public String indexPage(HttpServletRequest request, Model model) {
+        Map<String, Object> conditions = ParamMap.toMap(request.getParameterMap());
+
+        Paginator paginator = new Paginator(
+                request,
+                contentService.searchContentCount(conditions),
+                20);
+
+        List<Map<String, Object>> contents = contentService.searchContents(
+                conditions,
+                new ParamMap().add("createdTime", "DESC"),
+                paginator.getOffsetCount(),
+                paginator.getPerPageCount()
+        );
+
+        Set<Object> userIds = contents.stream().map(x -> x.get("userId")).collect(Collectors.toSet());
+        model.addAttribute("users", userService.findUsersByIds(userIds));
+
+        Set<Object> categoryIds = contents.stream().map(x -> x.get("categoryId")).collect(Collectors.toSet());
+        model.addAttribute("categories", categoryService.findCategoriesByIds(categoryIds));
+
+        model.addAttribute("paginator", paginator);
+        model.addAttribute("contents", contents);
         return "/admin/content/index";
     }
 }
