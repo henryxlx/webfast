fd9e87909294885a1e3a3a56cc2ea5a3f86e6ea2
xulixin <henryviolet@sina.com>
2022-01-02 12:47:00 +0000

    [Added] 查询结果数据集转换处理类ArrayToolkit重新使用Java8的流处理类ArrayToolkitJava8Util

---
+++ webfast-common/src/main/java/com/jetwinner/util/ArrayToolkitJava8Util.java
@@ -0,0 +1,32 @@
+package com.jetwinner.util;
+
+import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author xulixin
+ */
+public final class ArrayToolkitJava8Util {
+
+    private ArrayToolkitJava8Util() {
+        // reserved.
+    }
+
+    public static <T> Map<String, T> index(List<T> list, Function<T, Object> keyMapper) {
+        Map<Object, T> map = list.stream().collect(Collectors.toMap(keyMapper, Function.identity()));
+        Map<String, T> retMap = new HashMap<>(map.size());
+        map.forEach((k, v) -> retMap.put(String.valueOf(k), v));
+        return retMap;
+    }
+
+    public static <T> Set<Object> column(List<T> list, Function<T, Object> mapper) {
+        Assert.notNull(list, "ArrayToolkitJava8Util::column parameter list must not be null.");
+        return list.stream().map(mapper).collect(Collectors.toSet());
+    }
+}
