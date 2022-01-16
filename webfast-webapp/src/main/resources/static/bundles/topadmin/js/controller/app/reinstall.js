45bed11f1782fd541ef517d84bdde1faf73b907d
xulixin <henryviolet@sina.com>
2022-01-16 07:57:13 +0000

    [Changed] 判断Ajax的POST调用返回值为布尔型，成功返回为True，失败是False并做出相应处理

--- webfast-webapp/src/main/resources/static/bundles/topadmin/js/controller/app/reinstall.js
+++ webfast-webapp/src/main/resources/static/bundles/topadmin/js/controller/app/reinstall.js
@@ -5,9 +5,13 @@ define(function(require, exports, module) {
     exports.run = function () {
 
         $('#appReinstall').on('click', function () {
-            $.post($(this).data('url'), function (response) {
-                Notify.success('准备开始重新安装应用！');
-                window.location.reload();
+            $.post($(this).data('url'), function (retData) {
+                if (retData == true) {
+                    Notify.success('准备开始重新安装应用！');
+                    window.location.reload();
+                } else {
+                    Notify.danger('重新安装应用失败！');
+                }
             }).error(function (response) {
                 Notify.danger('重新安装应用失败！');
             });
