912e629538d90b3732f151ca1e907ec6ce3eaa54
xulixin <henryviolet@sina.com>
2022-01-21 14:33:47 +0000

    [Added] 添加Web控制器Controller的辅助工具类BaseControllerHelper

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/BaseControllerHelper.java
@@ -0,0 +1,38 @@
+package com.jetwinner.webfast.mvc;
+
+import com.jetwinner.util.ArrayUtil;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author xulixin
+ */
+public class BaseControllerHelper {
+
+    /**
+     * 创建消息提示响应
+     *
+     * @param  type        消息类型：info, warning, error
+     * @param  message     消息内容
+     * @param  title       消息抬头
+     * @param  duration    消息显示持续的时间
+     * @param  gotoUrl     消息跳转的页面
+     * @return Response
+     */
+    public static ModelAndView createMessageResponse(String type, String message, String title, int duration, String gotoUrl) {
+        if (!ArrayUtil.inArray(type, "info", "warning", "error")) {
+            throw new RuntimeException("type不正确");
+        }
+
+        ModelAndView mav = new ModelAndView("/default/message");
+        mav.addObject("type", type);
+        mav.addObject("message", message);
+        mav.addObject("title", title);
+        mav.addObject("duration", duration);
+        mav.addObject("gotoUrl", gotoUrl);
+        return mav;
+    }
+
+    public static ModelAndView createMessageResponse(String type, String message) {
+        return createMessageResponse(type, message, "", 0, null);
+    }
+}
