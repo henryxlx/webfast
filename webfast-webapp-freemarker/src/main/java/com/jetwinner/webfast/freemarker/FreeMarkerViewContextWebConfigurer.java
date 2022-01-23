a6a0c313938f6bfb22956b7c9a1bdeff20c15cc4
xulixin <henryviolet@sina.com>
2022-01-23 08:09:57 +0000

    [Append] 使用配置方式在FreeMarkerViewContextWebConfigurer自定义规则访问外部静态资源（覆盖addResourceHandlers方法）

--- webfast-webapp-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FreeMarkerViewContextWebConfigurer.java
+++ webfast-webapp-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FreeMarkerViewContextWebConfigurer.java
@@ -1,7 +1,9 @@
 package com.jetwinner.webfast.freemarker;
 
+import com.jetwinner.webfast.kernel.FastAppConst;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,16 +13,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class FreeMarkerViewContextWebConfigurer implements WebMvcConfigurer {
 
     private final FreeMarkerViewReferenceInterceptor viewReferenceInterceptor;
+    private final FastAppConst appConst;
 
-    public FreeMarkerViewContextWebConfigurer(FreeMarkerViewReferenceInterceptor viewReferenceInterceptor) {
+    public FreeMarkerViewContextWebConfigurer(FreeMarkerViewReferenceInterceptor viewReferenceInterceptor,
+                                              FastAppConst appConst) {
+
         this.viewReferenceInterceptor = viewReferenceInterceptor;
+        this.appConst = appConst;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(viewReferenceInterceptor).addPathPatterns("/**")
                 .excludePathPatterns("/assets/**", "/bundles/**", "/images/**", "/themes/**")
+                .excludePathPatterns(appConst.getUploadPublicUrlPath() + "/**")
                 .excludePathPatterns("/install/**");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(appConst.getUploadPublicUrlPath() + "/**")
+                .addResourceLocations("file:" + appConst.getUploadPublicDirectory() + "/");
+    }
 }
