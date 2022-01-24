9c68d5fe57b64947bc8048213fbaad90a3aff195
xulixin <henryviolet@sina.com>
2022-01-24 11:07:23 +0000

    [Changed] 用户个人主页按用户id获取用户信息注入页面

--- webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/UserController.java
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.jetwinner.webfast.mvc.controller;
 
+import com.jetwinner.webfast.kernel.service.AppUserService;
 import com.jetwinner.webfast.mvc.block.BlockRenderController;
 import com.jetwinner.webfast.mvc.block.BlockRenderMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller("webfastSiteUserController")
 public class UserController implements BlockRenderController {
 
+    private final AppUserService userService;
+
+    public UserController(AppUserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/user/{id}")
-    public String userHomePage(@PathVariable Integer id) {
+    public String userHomePage(@PathVariable Integer id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
         return "/user/user-home";
     }
 
