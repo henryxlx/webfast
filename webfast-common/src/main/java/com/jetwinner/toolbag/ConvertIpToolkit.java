a0bc241f03f3dcbe00e239c0770ce4106f5a06b3
xulixin <henryviolet@sina.com>
2022-01-25 14:34:01 +0000

    [Append] 重载转换IP地址的方法convertIp，可批量转换List<Map>中的ip地址，Map主键的名称需要单独指定

--- webfast-common/src/main/java/com/jetwinner/toolbag/ConvertIpToolkit.java
+++ webfast-common/src/main/java/com/jetwinner/toolbag/ConvertIpToolkit.java
@@ -1,11 +1,21 @@
 package com.jetwinner.toolbag;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author xulixin
  */
 public class ConvertIpToolkit {
 
     private static IpRealDataSeeker ipSeeker = IpRealDataSeeker.getInstance();
+
+    public static void convertIp(List<Map<String, Object>> list, String fieldName) {
+        list.forEach(e -> {
+            Object ip = e.get(fieldName);
+            e.put(fieldName, ip != null ? convertIp(String.valueOf(ip)) : "不能查询IP");
+        });
+    }
 
     public static String convertIp(String ip) {
         return ipSeeker == null ? "不能查询IP" : ipSeeker.getAddress(ip);
