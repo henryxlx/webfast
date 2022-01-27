3b405b512c5d97bcc7a761346b357292147a3ac3
xulixin <henryviolet@sina.com>
2022-01-27 15:17:06 +0000

    [Added] 添加消息关系值对象AppModelMessageRelation

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/kernel/model/AppModelMessageRelation.java
@@ -0,0 +1,44 @@
+package com.jetwinner.webfast.kernel.model;
+
+/**
+ * @author xulixin
+ */
+public class AppModelMessageRelation {
+
+    private Integer id;
+    private Integer conversationId;
+    private Integer messageId;
+    private Integer isRead;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(Integer conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Integer messageId) {
+        this.messageId = messageId;
+    }
+
+    public Integer getIsRead() {
+        return isRead;
+    }
+
+    public void setIsRead(Integer isRead) {
+        this.isRead = isRead;
+    }
+}
