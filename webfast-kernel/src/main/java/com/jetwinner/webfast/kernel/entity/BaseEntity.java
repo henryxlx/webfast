7bf5e2413c2ca9dcece3cc42b7c83769bff64c44
xulixin <henryviolet@sina.com>
2022-01-28 16:47:32 +0000

    [Add] 用于持久化PO对象的基类定义

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/kernel/entity/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.jetwinner.webfast.kernel.entity;
+
+import java.util.Date;
+
+/**
+ * @author xulixin
+ */
+public class BaseEntity {
+
+    private Integer id;
+    private Date createdTime;
+    private Integer createdUserId;
+    private Date updatedTime;
+    private Integer updatedUserId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Integer getCreatedUserId() {
+        return createdUserId;
+    }
+
+    public void setCreatedUserId(Integer createdUserId) {
+        this.createdUserId = createdUserId;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    public Integer getUpdatedUserId() {
+        return updatedUserId;
+    }
+
+    public void setUpdatedUserId(Integer updatedUserId) {
+        this.updatedUserId = updatedUserId;
+    }
+}
