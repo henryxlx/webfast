002e134d070e4839af985f34825cd3b6a9d78167
xulixin <henryviolet@sina.com>
2022-02-02 04:33:39 +0000

    [Changed] 配置服务类中的获取配置值的方法get设置缓存注解。

--- webfast-kernel-service-impl/src/main/java/com/jetwinner/webfast/kernel/service/AppSettingServiceImpl.java
+++ webfast-kernel-service-impl/src/main/java/com/jetwinner/webfast/kernel/service/AppSettingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jetwinner.webfast.kernel.service;
 
 import com.jetwinner.webfast.kernel.dao.AppSettingDao;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -23,6 +24,7 @@ public class AppSettingServiceImpl implements AppSettingService {
     }
 
     @Override
+    @Cacheable(value = "settingCache", key = "#name")
     public Map<String, Object> get(String name) {
         return settingDao.getSettingValueByName(name);
     }
