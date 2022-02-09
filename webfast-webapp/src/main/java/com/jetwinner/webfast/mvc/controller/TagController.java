a33f509567a62adb1b74c41d91dd89c21da6ee60
xulixin <henryviolet@sina.com>
2022-02-09 02:18:39 +0000

    [Added] 后台资讯管理添加新资讯获取应用程序标签请求的实现

---
+++ webfast-webapp/src/main/java/com/jetwinner/webfast/mvc/controller/TagController.java
@@ -0,0 +1,36 @@
+package com.jetwinner.webfast.mvc.controller;
+
+import com.jetwinner.webfast.kernel.service.AppTagService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xulixin
+ */
+@Controller("webfastSiteTagController")
+public class TagController {
+
+    private final AppTagService tagService;
+
+    public TagController(AppTagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @RequestMapping("/tag")
+    public String indexPage() {
+        return "/tag/index";
+    }
+
+    @RequestMapping("/tag/match_jsonp")
+    @ResponseBody
+    public List<Map<String, Object>> matchAction(@RequestParam(name = "q") String queryString,
+                                                 @RequestParam(required = false) String callback) {
+
+        return tagService.getTagByLikeName(queryString);
+    }
+}
