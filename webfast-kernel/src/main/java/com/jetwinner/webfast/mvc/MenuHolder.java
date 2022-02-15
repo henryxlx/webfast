3cf65a0bb956efcb562a888b89303e4b8ff6617d
xulixin <henryviolet@sina.com>
2022-02-15 13:54:40 +0000

    [Added] 后台管理顶部菜单缓存容器，由MenuConfigurer配置在启动时创建

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/MenuHolder.java
@@ -0,0 +1,19 @@
+package com.jetwinner.webfast.mvc;
+
+import java.util.List;
+
+/**
+ * @author xulixin
+ */
+public class MenuHolder {
+
+    private List<FastMenuEntry> adminTop;
+
+    public List<FastMenuEntry> getAdminTop() {
+        return adminTop;
+    }
+
+    public void setAdminTop(List<FastMenuEntry> adminTop) {
+        this.adminTop = adminTop;
+    }
+}
