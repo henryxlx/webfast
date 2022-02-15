8ef30427af46340a5ea710ada14dd494c398b481
xulixin <henryviolet@sina.com>
2022-02-15 13:52:54 +0000

    [Added] 创建后台顶部菜单使用的快速菜单项

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/FastMenuEntry.java
@@ -0,0 +1,37 @@
+package com.jetwinner.webfast.mvc;
+
+/**
+ * @author xulixin
+ */
+public class FastMenuEntry {
+
+    private String key;
+
+    private String title;
+
+    private String url;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
