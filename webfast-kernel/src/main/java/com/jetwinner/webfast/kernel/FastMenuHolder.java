919493b9f3a592f7325b162a7976d718c786c002
xulixin <henryviolet@sina.com>
2022-02-16 08:09:03 +0000

    [Added] 添加用于保存外部配置菜单信息的持有类FastMenuHolder

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/kernel/FastMenuHolder.java
@@ -0,0 +1,20 @@
+package com.jetwinner.webfast.kernel;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xulixin
+ */
+public class FastMenuHolder {
+
+    private List<Map<String, Object>> adminTop;
+
+    public List<Map<String, Object>> getAdminTop() {
+        return adminTop;
+    }
+
+    public void setAdminTop(List<Map<String, Object>> adminTop) {
+        this.adminTop = adminTop;
+    }
+}
