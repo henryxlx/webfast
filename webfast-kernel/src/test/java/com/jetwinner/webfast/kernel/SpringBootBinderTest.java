6770209a6fbf19cfa2538e3c96d775c72c05837d
xulixin <henryviolet@sina.com>
2022-02-16 11:05:08 +0000

    [Added] 试验SpringBoot中Binder类用法的测试类

---
+++ webfast-kernel/src/test/java/com/jetwinner/webfast/kernel/SpringBootBinderTest.java
@@ -0,0 +1,44 @@
+package com.jetwinner.webfast.kernel;
+
+import org.junit.Test;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
+
+public class SpringBootBinderTest {
+
+    @Test
+    public void testForDataDictYmlBinder() {
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("datadict-test.yml"));
+        Properties properties = yaml.getObject();
+        MapConfigurationPropertySource source = new MapConfigurationPropertySource(properties);
+        Binder binder = new Binder(source);
+        DataDictHolder dictHolder = binder.bind("data", DataDictHolder.class).get();
+        if (properties == null) {
+            assertTrue("yml file not found.", dictHolder.getDict() == null);
+        } else {
+            assertTrue("bind success!", dictHolder.getDict() != null);
+        }
+    }
+
+    @Test
+    public void testForAdminMenuYmlBinder() {
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("menu-admin-test.yml"));
+        Properties properties = yaml.getObject();
+        MapConfigurationPropertySource source = new MapConfigurationPropertySource(properties);
+        Binder binder = new Binder(source);
+        FastMenuHolder menuHolder = binder.bind("menu", FastMenuHolder.class).get();
+        if (properties == null) {
+            assertTrue("yml file not found.", menuHolder.getAdminTop() == null);
+        } else {
+            assertTrue("bind success!", menuHolder.getAdminTop() != null);
+        }
+    }
+}
