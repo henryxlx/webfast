f0b0961eee7221e43f1adb08072bd4585af7b030
xulixin <henryviolet@sina.com>
2022-02-18 05:27:03 +0000

    [Changed] 核心功能使用数据库只保留与核心功能相关的数据表

--- webfast-kernel-dao-impl-jdbc/src/main/resources/sql/mysql/webfast-kernel.sql
+++ webfast-kernel-dao-impl-jdbc/src/main/resources/sql/mysql/webfast-kernel.sql
@@ -1,53 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `app_article`
--- ----------------------------
-DROP TABLE IF EXISTS `app_article`;
-CREATE TABLE `app_article` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章ID',
-  `title` varchar(255) NOT NULL COMMENT '文章标题',
-  `categoryId` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '栏目',
-  `tagIds` tinytext COMMENT 'tag标签',
-  `source` varchar(1024) DEFAULT '' COMMENT '来源',
-  `sourceUrl` varchar(1024) DEFAULT '' COMMENT '来源URL',
-  `publishedTime` bigint unsigned NOT NULL DEFAULT '0' COMMENT '发布时间',
-  `body` text COMMENT '正文',
-  `thumb` varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图',
-  `originalThumb` varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图原图',
-  `picture` varchar(255) NOT NULL DEFAULT '' COMMENT '文章头图，文章编辑／添加时，自动取正文的第１张图',
-  `status` enum('published','unpublished','trash') NOT NULL DEFAULT 'unpublished' COMMENT '状态',
-  `hits` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '点击量',
-  `featured` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否头条',
-  `promoted` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '推荐',
-  `sticky` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否置顶',
-  `userId` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '文章发布人的ID',
-  `createdTime` bigint unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `updatedTime` bigint unsigned NOT NULL DEFAULT '0' COMMENT '最后更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for `app_article_category`
--- ----------------------------
-DROP TABLE IF EXISTS `app_article_category`;
-CREATE TABLE `app_article_category` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT '栏目名称',
-  `code` varchar(64) NOT NULL COMMENT 'URL目录名称',
-  `weight` int(11) NOT NULL DEFAULT '0',
-  `publishArticle` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否允许发布文章',
-  `seoTitle` varchar(1024) NOT NULL DEFAULT '' COMMENT '栏目标题',
-  `seoKeyword` varchar(1024) NOT NULL DEFAULT '' COMMENT 'SEO 关键字',
-  `seoDesc` varchar(1024) NOT NULL DEFAULT '' COMMENT '栏目描述（SEO）',
-  `published` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否启用（1：启用 0：停用)',
-  `parentId` int(11) unsigned NOT NULL DEFAULT '0',
-  `createdTime` bigint unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `code` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
 -- Table structure for `app_block`
 -- ----------------------------
 DROP TABLE IF EXISTS `app_block`;
@@ -80,64 +33,6 @@ CREATE TABLE `app_block_history` (
   `createdTime` bigint NOT NULL COMMENT '编辑区历史记录创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='编辑区历史表';
-
--- ----------------------------
--- Table structure for `app_category`
--- ----------------------------
-DROP TABLE IF EXISTS `app_category`;
-CREATE TABLE `app_category` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-  `code` varchar(64) NOT NULL DEFAULT '' COMMENT '分类编码',
-  `name` varchar(255) NOT NULL COMMENT '分类名称',
-  `icon` varchar(255) NOT NULL DEFAULT '' COMMENT '图标',
-  `path` varchar(255) NOT NULL DEFAULT '' COMMENT '分类完整路径',
-  `weight` int(11) NOT NULL DEFAULT '0' COMMENT '分类权重',
-  `groupId` int(11) unsigned NOT NULL COMMENT '分类组ID',
-  `parentId` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '父分类ID',
-  `description` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uri` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分类表';
-
--- ----------------------------
--- Table structure for `app_category_group`
--- ----------------------------
-DROP TABLE IF EXISTS `app_category_group`;
-CREATE TABLE `app_category_group` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类组ID',
-  `code` varchar(64) NOT NULL COMMENT '分类组编码',
-  `name` varchar(255) NOT NULL COMMENT '分类组名称',
-  `depth` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '该组下分类允许的最大层级数',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分类分组表';
-
--- ----------------------------
--- Table structure for `app_content`
--- ----------------------------
-DROP TABLE IF EXISTS `app_content`;
-CREATE TABLE `app_content` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '内容ID',
-  `title` varchar(255) NOT NULL COMMENT '内容标题',
-  `editor` enum('richeditor','none') NOT NULL DEFAULT 'richeditor' COMMENT '编辑器选择类型字段',
-  `type` varchar(255) NOT NULL COMMENT '内容类型',
-  `alias` varchar(255) NOT NULL DEFAULT '' COMMENT '内容别名',
-  `summary` text COMMENT '内容摘要',
-  `body` text COMMENT '内容正文',
-  `picture` varchar(255) NOT NULL DEFAULT '' COMMENT '内容头图',
-  `template` varchar(255) NOT NULL DEFAULT '' COMMENT '内容模板',
-  `status` enum('published','unpublished','trash') NOT NULL COMMENT '内容状态',
-  `categoryId` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '内容分类ID',
-  `tagIds` tinytext COMMENT '内容标签ID',
-  `hits` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '内容点击量',
-  `featured` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '是否头条',
-  `promoted` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否推荐',
-  `sticky` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否置顶',
-  `userId` int(11) unsigned NOT NULL COMMENT '发布人ID',
-  `extraField` text COMMENT '扩展字段',
-  `publishedTime` bigint NOT NULL DEFAULT '0' COMMENT '发布时间',
-  `createdTime` bigint  NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='页面内容表';
 
 -- ----------------------------
 -- Table structure for `app_file`
@@ -334,18 +229,6 @@ INSERT INTO `app_setting` VALUES ('3', 'mailer', 0x613A373A7B733A373A22656E61626
 INSERT INTO `app_setting` VALUES ('4', 'storage', 0x613A353A7B733A31313A2275706C6F61645F6D6F6465223B733A353A226C6F63616C223B733A31363A22636C6F75645F6163636573735F6B6579223B733A303A22223B733A31363A22636C6F75645F7365637265745F6B6579223B733A303A22223B733A31363A22636C6F75645F6170695F736572766572223B733A32323A22687474703A2F2F6170692E656475736F686F2E6E6574223B733A31323A22636C6F75645F6275636B6574223B733A303A22223B7D);
 INSERT INTO `app_setting` VALUES ('5', 'theme', 0x613A313A7B733A333A22757269223B733A373A2264656661756C74223B7D);
 INSERT INTO `app_setting` VALUES ('6', 'article', 0x613A323A7B733A343A226E616D65223B733A31323A22E8B584E8AEAFE9A291E98193223B733A383A22706167654E756D73223B693A32303B7D);
-
--- ----------------------------
--- Table structure for `app_tag`
--- ----------------------------
-DROP TABLE IF EXISTS `app_tag`;
-CREATE TABLE `app_tag` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '标签ID',
-  `name` varchar(64) NOT NULL COMMENT '标签名称',
-  `createdTime` bigint unsigned NOT NULL COMMENT '标签创建时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应用标签表';
 
 -- ----------------------------
 -- Table structure for `app_user`
