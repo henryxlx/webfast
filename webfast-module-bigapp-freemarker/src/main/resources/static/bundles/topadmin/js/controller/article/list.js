fc8bf3f29cda43eba400fa994a731f85478a3ac8
xulixin <henryviolet@sina.com>
2022-02-25 12:36:55 +0000

    [Polished] 后台资讯管理发布、取消发布、移至回收站和删除操作添加顶部提示消息

--- webfast-module-bigapp-freemarker/src/main/resources/static/bundles/topadmin/js/controller/article/list.js
+++ webfast-module-bigapp-freemarker/src/main/resources/static/bundles/topadmin/js/controller/article/list.js
@@ -1,21 +1,43 @@
 define(function(require, exports, module) {
+	var Notify = require('common/bootstrap-notify');
 
 	exports.run = function() {
 		$("#article-table").on('click', '[data-role=trash-item]', function(){
-			$.post($(this).data('url'), function(){
-				window.location.reload();
+			$.post($(this).data('url'), function (retData) {
+				if (retData == true) {
+					Notify.success('资讯移至回收站成功！');
+					setTimeout(function () { window.location.reload(); }, 1000);
+				} else {
+					Notify.danger('资讯移至回收站失败！');
+				}
+			}).error(function (response) {
+				Notify.danger('资讯移至回收站时发生错误，请检查后重试！');
 			});
 		});
 
 		$("#article-table").on('click', '[data-role=publish-item]', function(){
-			$.post($(this).data('url'), function(){
-				window.location.reload();
+			$.post($(this).data('url'), function (retData) {
+				if (retData == true) {
+					Notify.success('资讯发布成功！');
+					setTimeout(function () { window.location.reload(); }, 1000);
+				} else {
+					Notify.danger('资讯发布失败！');
+				}
+			}).error(function (response) {
+				Notify.danger('资讯发布时发生错误，请检查后重试！');
 			});
 		});	
 
 		$("#article-table").on('click', '[data-role=unpublish-item]', function(){
-			$.post($(this).data('url'), function(){
-				window.location.reload();
+			$.post($(this).data('url'), function (retData) {
+				if (retData == true) {
+					Notify.success('资讯取消发布成功！');
+					setTimeout(function () { window.location.reload(); }, 1000);
+				} else {
+					Notify.danger('资讯取消发布失败！');
+				}
+			}).error(function (response) {
+				Notify.danger('资讯取消发布时发生错误，请检查后重试！');
 			});
 		});	
 
@@ -23,8 +45,15 @@ define(function(require, exports, module) {
 			if (!confirm('真的要永久删除该内容吗？')) {
 				return ;
 			}
-			$.post($(this).data('url'), function(){
-				window.location.reload();
+			$.post($(this).data('url'), function (retData) {
+				if (retData == true) {
+					Notify.success('删除资讯成功！');
+					setTimeout(function () { window.location.reload(); }, 1000);
+				} else {
+					Notify.danger('删除资讯失败！');
+				}
+			}).error(function (response) {
+				Notify.danger('删除资讯时发生错误，请检查后重试！');
 			});
 		});
 
