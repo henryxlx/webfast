ee78f690933ca577d968550c83d5a7a105fab2de
xulixin <henryviolet@sina.com>
2022-03-15 02:05:53 +0100

    [Changed] 在控制SpringBoot应用配置中注册网站离线和防止跨站请求伪造(CSRF)攻击的拦截器

--- webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FreeMarkerViewContextWebConfigurer.java
+++ webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FreeMarkerViewContextWebConfigurer.java
@@ -1,6 +1,8 @@
 package com.jetwinner.webfast.freemarker;
 
 import com.jetwinner.webfast.kernel.FastAppConst;
+import com.jetwinner.webfast.mvc.WebAppOfflineInterceptor;
+import com.jetwinner.webfast.mvc.extension.csrf.CsrfValidationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,18 +15,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class FreeMarkerViewContextWebConfigurer implements WebMvcConfigurer {
 
     private final FreeMarkerViewReferenceInterceptor viewReferenceInterceptor;
+    private final WebAppOfflineInterceptor offlineInterceptor;
     private final FastAppConst appConst;
 
     public FreeMarkerViewContextWebConfigurer(FreeMarkerViewReferenceInterceptor viewReferenceInterceptor,
+                                              WebAppOfflineInterceptor offlineInterceptor,
                                               FastAppConst appConst) {
 
         this.viewReferenceInterceptor = viewReferenceInterceptor;
+        this.offlineInterceptor = offlineInterceptor;
         this.appConst = appConst;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(viewReferenceInterceptor).addPathPatterns("/**")
+        registry.addInterceptor(offlineInterceptor).order(0).addPathPatterns("/*");
+        registry.addInterceptor(new CsrfValidationInterceptor()).order(1).addPathPatterns("/*");
+        registry.addInterceptor(viewReferenceInterceptor).order(9999).addPathPatterns("/**")
                 .excludePathPatterns("/assets/**", "/bundles/**", "/images/**", "/themes/**")
                 .excludePathPatterns(appConst.getUploadPublicUrlPath() + "/**")
                 .excludePathPatterns("/install/**");
