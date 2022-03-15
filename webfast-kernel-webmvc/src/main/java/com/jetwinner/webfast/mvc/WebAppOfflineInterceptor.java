ab65376ef1df310c5b42ca4370451a53e4f716ea
xulixin <henryviolet@sina.com>
2022-03-15 07:05:22 +0100

    [Changed] 离线时获取静态页面直接在response中显示，管理员和超级管理员登录后可正常使用

--- webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/WebAppOfflineInterceptor.java
+++ webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/WebAppOfflineInterceptor.java
@@ -1,14 +1,18 @@
 package com.jetwinner.webfast.mvc;
 
+import com.jetwinner.security.UserAccessControlService;
 import com.jetwinner.webfast.kernel.FastAppConst;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author xulixin
@@ -19,26 +23,26 @@ public class WebAppOfflineInterceptor extends HandlerInterceptorAdapter {
     private static final Logger logger = LoggerFactory.getLogger(WebAppOfflineInterceptor.class);
 
     private final FastAppConst appConst;
+    private final UserAccessControlService userAccessControlService;
 
-    public WebAppOfflineInterceptor(FastAppConst appConst) {
+    public WebAppOfflineInterceptor(FastAppConst appConst, UserAccessControlService userAccessControlService) {
         this.appConst = appConst;
+        this.userAccessControlService = userAccessControlService;
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (appConst.getOffline()) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        if (appConst.getOffline() && !userAccessControlService.hasAnyRole("ROLE_ADMIN", "ROLE_SUPER_ADMIN")) {
+            response.setContentType("text/html; charset=" + StandardCharsets.UTF_8);
+            OutputStream out = response.getOutputStream();
+            ClassPathResource resource = new ClassPathResource("/static/offline.html");
+            FileCopyUtils.copy(resource.getInputStream(), out);
+            out.flush();
             return false;
         }
         return true;
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           ModelAndView mav)
-            throws Exception {
-
-        if (appConst.getOffline()) {
-            mav.setViewName("/offline");
-        }
-    }
 }
