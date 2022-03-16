93dc4ca12e37b8e320f3a217b015ae8ac889d127
xulixin <henryviolet@sina.com>
2022-03-16 09:56:12 +0100

    [Changed] 修改个人介绍与关注/粉丝处理功能

--- webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/UserController.java
+++ webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.jetwinner.webfast.mvc.controller;
 
+import com.jetwinner.webfast.kernel.AppUser;
+import com.jetwinner.webfast.kernel.exception.RuntimeGoingException;
 import com.jetwinner.webfast.kernel.service.AppUserService;
 import com.jetwinner.webfast.mvc.block.BlockRenderController;
 import com.jetwinner.webfast.mvc.block.BlockRenderMethod;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author xulixin
@@ -21,22 +25,52 @@ public class UserController implements BlockRenderController {
         this.userService = userService;
     }
 
-    @RequestMapping("/user/{id}")
+    @RequestMapping({"/user/{id}", "/user/{id}/about"})
     public String userHomePage(@PathVariable Integer id, Model model) {
         model.addAttribute("user", userService.getUser(id));
-        return "/user/user-home";
+        model.addAttribute("userProfile", userService.getUserProfile(id));
+        return "/user/about";
     }
 
-    @RequestMapping("/user/{id}/{pageNav}")
-    public String userHomeTagPage(@PathVariable Integer id, @PathVariable String pageNav, Model model) {
+    @RequestMapping("/user/{id}/group")
+    public String userHomeTagPage(@PathVariable Integer id, HttpServletRequest request, Model model) {
+        String pathInfo = request.getServletPath();
+        String pageNav = pathInfo.substring(pathInfo.lastIndexOf('/') + 1);
         model.addAttribute("pageNav", pageNav);
         model.addAttribute("user", userService.getUser(id));
         return "/user/user-home";
     }
 
+    @RequestMapping("/user/{id}/following")
+    private String followingAction(@PathVariable Integer id, Model model) {
+        AppUser user = tryGetUser(id);
+        //model.addAttribute("friends", userService.findAllUserFollowing(user.getId()));
+        model.addAttribute("user", user);
+        model.addAttribute("friendNav", "following");
+        return "/user/friend";
+    }
+
+    @RequestMapping("/user/{id}/follower")
+    public String followerAction(@PathVariable Integer id, Model model) {
+        AppUser user = tryGetUser(id);
+        //model.addAttribute("friends", userService.findAllUserFollower(user.getId()));
+        model.addAttribute("user", user);
+        model.addAttribute("friendNav", "follower");
+        return "/user/friend";
+    }
+
+    private AppUser tryGetUser(Integer id) {
+        AppUser user = userService.getUser(id);
+        if (user == null) {
+            throw new RuntimeGoingException("用户未找到！");
+        }
+        return user;
+    }
+
     @GetMapping("/user/headerBlock")
     @BlockRenderMethod
-    public String showHeaderBlock() {
+    public String showHeaderBlock(AppUser user, Model model) {
+        model.addAttribute("userProfile", userService.getUserProfile(user.getId()));
         return "/user/header-block";
     }
 }
