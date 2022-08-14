601882b0e5656c637cb3a37c688e7416ce86df97
xulixin <henryviolet@sina.com>
2022-08-14 13:05:52 +0100

    [Polish] 消除Magic Value，定义常量代替，补全缺少的文档注释

--- webfast-common/src/main/java/com/jetwinner/servlet/RequestIpAddressUtil.java
+++ webfast-common/src/main/java/com/jetwinner/servlet/RequestIpAddressUtil.java
@@ -11,23 +11,21 @@ import java.io.IOException;
  */
 public final class RequestIpAddressUtil {
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestIpAddressUtil.class);
+
+    private static final String IP_UNKNOWN = "unknown";
+
     private RequestIpAddressUtil() {
         // reserved.
     }
 
     /**
-     * Logger for this class
-     */
-    private static Logger logger = LoggerFactory.getLogger(RequestIpAddressUtil.class);
-
-    /**
      * 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址;
      *
-     * @param request
-     * @return
-     * @throws IOException
+     * @param request 客户端请求
+     * @return 客户端IP地址字符串
      */
-    public final static String getClientIp(HttpServletRequest request) {
+    public static String getClientIp(HttpServletRequest request) {
         try {
             return getIpAddress(request);
         } catch (IOException e) {
@@ -35,7 +33,7 @@ public final class RequestIpAddressUtil {
         }
     }
 
-    public final static String getIpAddress(HttpServletRequest request) throws IOException {
+    public static String getIpAddress(HttpServletRequest request) throws IOException {
         // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
 
         String ip = request.getHeader("X-Forwarded-For");
@@ -43,48 +41,47 @@ public final class RequestIpAddressUtil {
             logger.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
         }
 
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
                 ip = request.getHeader("Proxy-Client-IP");
                 if (logger.isInfoEnabled()) {
                     logger.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
                 }
             }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
                 ip = request.getHeader("WL-Proxy-Client-IP");
                 if (logger.isInfoEnabled()) {
                     logger.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
                 }
             }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
                 ip = request.getHeader("HTTP_CLIENT_IP");
                 if (logger.isInfoEnabled()) {
                     logger.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
                 }
             }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
                 ip = request.getHeader("HTTP_X_FORWARDED_FOR");
                 if (logger.isInfoEnabled()) {
                     logger.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
                 }
             }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
                 ip = request.getRemoteAddr();
                 if (logger.isInfoEnabled()) {
-                    logger.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
+                    logger.info("getIpAddress(HttpServletRequest) - getRemoteAddress - String ip=" + ip);
                 }
             }
         } else if (ip.length() > 15) {
             String[] ips = ip.split(",");
-            for (int index = 0; index < ips.length; index++) {
-                String strIp = (String) ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
+            for (String strIp : ips) {
+                if (!(IP_UNKNOWN.equalsIgnoreCase(strIp))) {
                     ip = strIp;
                     break;
                 }
             }
         }
-        if (ip.equals("0:0:0:0:0:0:0:1")) {
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
             ip = "127.0.0.1";
         }
         return ip;
