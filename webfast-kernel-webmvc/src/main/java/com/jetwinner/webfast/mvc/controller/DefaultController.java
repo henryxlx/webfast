002068c16a59f6e1f77965321889d033b754bb56
xulixin <henryviolet@sina.com>
2023-02-25 00:30:37 +0000

    feat: 前台首页使用新定义的接口PageActionModelService，用于扩展网站首页模型中新增其它数据

--- webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/DefaultController.java
+++ webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/DefaultController.java
@@ -2,8 +2,10 @@ package com.jetwinner.webfast.mvc.controller;
 
 import com.jetwinner.webfast.kernel.service.AppBlockService;
 import com.jetwinner.webfast.kernel.service.AppNavigationService;
+import com.jetwinner.webfast.mvc.PageActionModelService;
 import com.jetwinner.webfast.mvc.block.BlockRenderController;
 import com.jetwinner.webfast.mvc.block.BlockRenderMethod;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +18,29 @@ public class DefaultController implements BlockRenderController {
 
     private final AppBlockService blockService;
     private final AppNavigationService navigationService;
+    private final ApplicationContext applicationContext;
 
-    public DefaultController(AppBlockService blockService, AppNavigationService navigationService) {
+    public DefaultController(AppBlockService blockService,
+                             AppNavigationService navigationService,
+                             ApplicationContext applicationContext) {
+
         this.blockService = blockService;
         this.navigationService = navigationService;
+        this.applicationContext = applicationContext;
     }
 
     @RequestMapping({"/", "/index"})
     public String index(Model model) {
         model.addAttribute("home_top_banner", blockService.getBlockByCode("home_top_banner"));
+        String[] beanNames = applicationContext.getBeanNamesForType(PageActionModelService.class);
+        if (beanNames != null) {
+            for (String beanName : beanNames) {
+                PageActionModelService indexPageAction =
+                        (PageActionModelService) applicationContext.getBean(beanName);
+                indexPageAction.addMoreObject(model);
+            }
+        }
+
         return "/default/index";
     }
 
