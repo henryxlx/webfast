6601b666eab5f157c193504dc3639540f6bb5093
xulixin <henryviolet@sina.com>
2023-02-25 00:25:01 +0000

    feat: 新增页面行为模型数据注入服务接口PageActionModelService，用于扩展首页添加的额外数据的功能

---
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/PageActionModelService.java
@@ -0,0 +1,11 @@
+package com.jetwinner.webfast.mvc;
+
+import org.springframework.ui.Model;
+
+/**
+ * @author xulixin
+ */
+public interface PageActionModelService {
+
+    public void addMoreObject(Model model);
+}
