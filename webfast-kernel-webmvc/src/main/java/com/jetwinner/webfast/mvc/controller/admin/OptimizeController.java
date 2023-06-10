6186d7769f7ad798cdaa6f2577c730aaaf6520e8
xulixin <henryviolet@sina.com>
2023-06-10 04:07:32 +0100

    feat: 后台管理中的系统优化功能OptimizeController实现对数据库中数据表的备份与下载功能backupDbAction

--- webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/admin/OptimizeController.java
+++ webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/controller/admin/OptimizeController.java
@@ -1,10 +1,16 @@
 package com.jetwinner.webfast.mvc.controller.admin;
 
 import com.jetwinner.util.FastDirectoryUtil;
+import com.jetwinner.util.PhpStringUtil;
+import com.jetwinner.webfast.database.FastDatabaseUtil;
 import com.jetwinner.webfast.kernel.FastAppConst;
+import com.jetwinner.webfast.kernel.typedef.ParamMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author xulixin
@@ -21,11 +27,19 @@ public class OptimizeController {
     @RequestMapping("/admin/optimize/remove-tmp")
     @ResponseBody
     public Boolean removeTempDirAction() {
-        if(!this.isDisabledUpgrade()){
+        if (!this.isDisabledUpgrade()) {
             FastDirectoryUtil.emptyDir(appConst.getDownloadPath());
         }
         FastDirectoryUtil.emptyDir(appConst.getUploadTmpPath());
         return Boolean.TRUE;
+    }
+
+    @RequestMapping("/admin/optimize/backupdb")
+    @ResponseBody
+    public Map<String, Object> backupDbAction(HttpServletRequest request) {
+        String db = FastDatabaseUtil.backupdb(appConst.getUploadTmpPath());
+        String downloadFile = request.getContextPath() + "/files/" + PhpStringUtil.basename(db);
+        return new ParamMap().add("status", "ok").add("result", downloadFile).toMap();
     }
 
     private boolean isDisabledUpgrade() {
