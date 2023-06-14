b6fb67ede22a623937a777aeddca9682b4b37ec1
xulixin <henryviolet@sina.com>
2023-06-14 05:33:49 +0100

    feat: FreeMarker页面公共对象FastLibraryObject添加计算时间长度的方法remainTime

--- webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FastLibraryObject.java
+++ webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FastLibraryObject.java
@@ -13,4 +13,22 @@ public class FastLibraryObject {
         }
         return 0;
     }
+
+    public String remainTime(Long value) {
+        long remain = value - System.currentTimeMillis();
+
+        if (remain <= 0) {
+            return "0分钟";
+        }
+
+        if (remain <= 3600000) {
+            return (remain / 60) + "分钟";
+        }
+
+        if (remain < 86400000) {
+            return (remain / 3600000) + "小时";
+        }
+
+        return (remain / 86400000) + "天";
+    }
 }
