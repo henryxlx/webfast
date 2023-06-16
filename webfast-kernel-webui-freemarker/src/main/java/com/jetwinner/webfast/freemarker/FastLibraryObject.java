ced04121b9c52bccba39a98a7aea9ba853469e3c
xulixin <henryviolet@sina.com>
2023-06-16 14:28:19 +0100

    feat: FreeMarker页面公共对象FastLibraryObject添加文件长度的方法fileSize，灵活显示时间的方法smartTime和简化截断字符串的方法plainText

--- webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FastLibraryObject.java
+++ webfast-kernel-webui-freemarker/src/main/java/com/jetwinner/webfast/freemarker/FastLibraryObject.java
@@ -1,5 +1,11 @@
 package com.jetwinner.webfast.freemarker;
 
+import com.jetwinner.util.EasyStringUtil;
+import com.jetwinner.util.FastTimeUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author xulixin
  */
@@ -9,7 +15,7 @@ public class FastLibraryObject {
 
     Integer percent(Integer part, Integer whole) {
         if (part != null && whole != null) {
-            return (int) (part.intValue() * 1.0 / whole.intValue() * 1.0 * 100);
+            return (int) (part * 1.0 / whole * 1.0 * 100);
         }
         return 0;
     }
@@ -36,5 +42,72 @@ public class FastLibraryObject {
         long minutes = value / (60 * 1000);
         long seconds = (value - minutes * 60 * 1000) / 1000;
         return String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+    }
+
+    public String plainText(String text) {
+        if (text != null) {
+            return EasyStringUtil.plainTextFilter(text);
+        }
+        return text;
+    }
+
+    public String plainText(String text, int length) {
+        if (text != null && text.length() > length) {
+            return EasyStringUtil.plainTextFilter(text, length);
+        }
+        return text;
+    }
+
+    public String fileSize(long size) {
+        double currentValue = 0.0;
+        String currentUnit = "";
+        Map<String, Double> unitExps = new HashMap<>(4);
+        unitExps.put("B", 0.0);
+        unitExps.put("KB", 1.0);
+        unitExps.put("MB", 2.0);
+        unitExps.put("GB", 3.0);
+        for (String unit : unitExps.keySet()) {
+            double exp = unitExps.get(unit);
+            double divisor = Math.pow(1000, exp);
+            currentUnit = unit;
+            currentValue = size / divisor;
+            if (currentValue < 1000) {
+                break;
+            }
+        }
+        return String.format("%.1f", currentValue) + currentUnit;
+    }
+
+    public String smartTime(long time) {
+        long diff = (System.currentTimeMillis() - time) / 1000;
+        if (diff < 0) {
+            return "未来";
+        }
+
+        if (diff == 0) {
+            return "刚刚";
+        }
+
+        if (diff < 60) {
+            return diff + "秒前";
+        }
+
+        if (diff < 3600) {
+            return Math.round(diff / 60) + "分钟前";
+        }
+
+        if (diff < 86400) {
+            return Math.round(diff / 3600) + "小时前";
+        }
+
+        if (diff < 2592000) {
+            return Math.round(diff / 86400) + "天前";
+        }
+
+        if (diff < 31536000) {
+            return FastTimeUtil.timeToDateTimeStr("MM-dd", time);
+        }
+
+        return FastTimeUtil.timeToDateTimeStr("yyyy-MM-dd", time);
     }
 }
