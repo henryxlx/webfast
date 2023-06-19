e2860b019932e1d4d16575628c67316ed3653ace
xulixin <henryviolet@sina.com>
2023-06-19 01:10:57 +0100

    doc: 添加BlockRenderController接口作用文档说明

--- webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/block/BlockRenderController.java
+++ webfast-kernel/src/main/java/com/jetwinner/webfast/mvc/block/BlockRenderController.java
@@ -1,6 +1,11 @@
 package com.jetwinner.webfast.mvc.block;
 
 /**
+ * 定义用于页面块Block逻辑处理的接口，空接口只是个标识，用于快速过滤和筛选
+ * 本接口主要用于在系统启动时扫描过滤带有此接口的控制器，获取其中带有@BlockRenderMethod注解方法的签名与URL地址映射，
+ * 供在页面上使用FreeMarker自定义宏<@renderController path=..../>通过path属性查找URL找到对应的带@BlockRenderMethod注解的方法，
+ * 完成相关代码逻辑功能的调用，这是一个基于FreeMarker自己实现的页面视图调用机制
+ *
  * @author xulixin
  */
 public interface BlockRenderController {
