3131bbcdf44190592907c0b433a7202b8bd4ea43
xulixin <henryviolet@sina.com>
2023-06-27 02:25:41 +0100

    fix: 删除fillQuestionStemText方法造成测试无效没有及时处理，保留对字符串replaceAll的正则表达式用法测试

--- webfast-kernel-webui-freemarker/src/test/java/com/jetwinner/webfast/freemarker/FastLibraryObjectTest.java
+++ webfast-kernel-webui-freemarker/src/test/java/com/jetwinner/webfast/freemarker/FastLibraryObjectTest.java
@@ -1,7 +1,6 @@
 package com.jetwinner.webfast.freemarker;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,12 +13,18 @@ public class FastLibraryObjectTest {
         fastLib = new FastLibraryObject();
     }
 
-    @Test
+    private String fillQuestionStemText(String stem) {
+        if (stem != null && stem.indexOf("[[") >= 0 && stem.indexOf("]]") >= 0) {
+            return stem.replaceAll("\\[\\[.+?\\]\\]", "____");
+        }
+        return stem;
+    }
+
     public void fillQuestionStemText() {
-        String str = fastLib.fillQuestionStemText("今年是[[2014|马]]年");
+        String str = fillQuestionStemText("今年是[[2014|马]]年");
         assertEquals("今年是____年", str);
         str = "《鲁滨逊漂流记》的作者是英国作家[[丹尼尔迪福]]。他在一座无人荒岛上生活多年后，收得一土人为奴，取名[[星期五]]。";
         String expect = "《鲁滨逊漂流记》的作者是英国作家____。他在一座无人荒岛上生活多年后，收得一土人为奴，取名____。";
-        assertEquals(expect, fastLib.fillQuestionStemText(str));
+        assertEquals(expect, fillQuestionStemText(str));
     }
 }
