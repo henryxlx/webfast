bd1d4cbaf0124988f2565b2b19a65fa9bb989f64
xulixin <henryviolet@sina.com>
2023-06-29 01:36:37 +0100

    fix: 升级SpringBoot 2.3.12.RELEASE后原来的ErrorController被废弃，使用SuppressWarning抑制

--- webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/WebAppGlobalErrorController.java
+++ webfast-kernel-webmvc/src/main/java/com/jetwinner/webfast/mvc/WebAppGlobalErrorController.java
@@ -1,9 +1,7 @@
 package com.jetwinner.webfast.mvc;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +18,7 @@ public class WebAppGlobalErrorController implements ErrorController {
     private final static String ERROR_PATH = "/error";
 
     @Override
+    @SuppressWarnings("deprecation")
     public String getErrorPath() {
         return ERROR_PATH;
     }
